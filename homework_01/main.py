c3e672f90d04b4d262776b15bcad8a1f558f2291
arting <thepublicefrag@ro.ru>
2023-01-09 22:09:01 +0300

    Creating the function filter_numbers and the function is_prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,16 +2,12 @@
 Домашнее задание №1
 Функции и структуры данных
 """
-
-
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
     """
-
+    return [x ** 2 for x in args]
 
 # filter types
 ODD = "odd"
@@ -19,14 +15,26 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(n):
+   if n <= 1 or n % 1 > 0:
+      return False
+   for i in range(2, n):
+      if n % i == 0:
+         return False
+   return True
+
+
+FILTERS_MAP = {
+    ODD: lambda n: n % 2 != 0,
+    EVEN: lambda n: n % 2 == 0,
+    PRIME: is_prime,
+}
+
+
+def filter_numbers(list_of_numbers, type_of_number):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
     """
+    return list(filter(FILTERS_MAP[type_of_number], list_of_numbers))
